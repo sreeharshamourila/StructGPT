a19d7419c339ac78bf97e6fc327a23cea94a0fc6
sreeharshamourila <sreeharsha.mourila@gmail.com>
2024-02-10 20:21:00 -0700

    Minor changes

--- DataCrafting/SocialMediaPostDataCrafting.py
+++ DataCrafting/SocialMediaPostDataCrafting.py
@@ -6,7 +6,7 @@ class SocialMediaPostDataCrafting(DataCrafting):
         data_copy = {key: value for key, value in data.items() if key != 'type'}
 
         data_copy['user_name'] = "NA" if data_copy.get('user_name') is None else data_copy.get('user_name')
-        data_copy['hashtags'] = "NA" if data_copy.get('hashtags') is None else "#"+"#".join(data_copy.get('hashtags'))
+        data_copy['hashtags'] = "NA" if data_copy.get('hashtags') is None else "#".join(data_copy.get('hashtags'))
         data_copy['content'] = "NA" if data_copy.get('content') is None else data_copy.get('content')
         # Now, use the modified copy of the data to instantiate the CustomerReview model
         return SocialMediaPost(**data_copy)
@@ -15,7 +15,7 @@ class SocialMediaPostDataCrafting(DataCrafting):
         # Extract hashtags for easier display, etc.
         return {
             "type": "social_media_post",
-            "user_name": "NA" if data["user_id"] is None else data["user_id"],
+            "user_name": "NA" if data["user_name"] is None else data["user_name"],
             "content": "NA" if data["content"] is None else data["content"],
-            "hashtags": "NA" if data["hashtags"] is None else "#"+ "#".join(data["hashtags"])  # Assuming hashtags are comma-separated
+            "hashtags": "NA" if data["hashtags"] is None else  "#".join(data["hashtags"])  # Assuming hashtags are comma-separated
         }
